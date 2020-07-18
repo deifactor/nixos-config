a7919ffcc693a0611ce6d929272ded0e3ab60077
Ash <ext0l@riseup.net>
2020-07-17 17:42:44 -0700

    Disable the firewall.

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,7 @@
   networking.hostName = "superluminal-steel"; # Define your hostname.
   networking.wireless.enable =
     true; # Enables wireless support via wpa_supplicant.
+  networking.firewall.enable = false;
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
